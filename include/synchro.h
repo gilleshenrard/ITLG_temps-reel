06b4daf193d6351462f2f771722f5172a9a87a42
Gilles Henrard <gilles.henrard@hotmail.com>
2021-02-16 17:47:46 +0000

    add error management to barrier_alloc

--- include/synchro.h
+++ include/synchro.h
@@ -4,16 +4,17 @@
 #include <semaphore.h>
 #include <inttypes.h>
 #include <stdlib.h>
+#include <errno.h>
 
 typedef struct{
-    pthread_mutex_t mutex;          //mutex used internally to protect the counters manipulations
-    sem_t           turnstile1;     //first semaphore used during the synchronisation
-    sem_t           turnstile2;     //second semaphore used during the synchronisation
-    uint16_t        th_count;       //Amount of threads currently waiting in the barrier sas
-    uint16_t        th_nb;          //Total amount of threads to synchronise in the barrier
+    pthread_mutex_t mutex;                          //mutex used internally to protect the counters manipulations
+    sem_t           turnstile1;                     //first semaphore used during the synchronisation
+    sem_t           turnstile2;                     //second semaphore used during the synchronisation
+    uint16_t        th_count;                       //Amount of threads currently waiting in the barrier sas
+    uint16_t        th_nb;                          //Total amount of threads to synchronise in the barrier
 }barrier_t;
 
-int barrier_alloc(barrier_t* bar);
+int barrier_alloc(barrier_t* bar, const uint16_t nb);
 int barrier_free(barrier_t* bar);
 int barrier_sync();
 
--- src/synchro.c
+++ src/synchro.c
@@ -9,17 +9,51 @@
 
 /****************************************************************************************/
 /*  I : Barrier type to allocate                                                        */
+/*      Amount of threads to synchronise                                                */
 /*  P : Allocate a Barrier type in memory (no value initialisation)                     */
 /*  O : 0 if ok                                                                         */
-/*     -1 otherwise                                                                     */
+/*     -1 otherwise and errno is set                                                    */
 /****************************************************************************************/
-int barrier_alloc(barrier_t* bar){
+int barrier_alloc(barrier_t* bar, const uint16_t nb){
+    uint16_t errtmp = 0;
+    
+    //allocate space for the structure
     bar = calloc(1, sizeof(barrier_t));
-    if (bar){
-        pthread_mutex_init(& bar->mutex, NULL);
-        sem_init (&bar->turnstile1, 0, 0);
-        sem_init (&bar->turnstile2, 0, 1);
+    if (!bar){
+        errno = ENOMEM;
+        return -1;
     }
+    
+    //initialise the internal mutex
+    if(pthread_mutex_init(& bar->mutex, NULL) < 0){
+        errtmp = errno;
+        free(bar);
+        errno = errtmp;
+        return -1;
+    }
+
+    //initialise the first semaphore used
+    if(sem_init(&bar->turnstile1, 0, 0) < 0){
+        errtmp = errno;
+        pthread_mutex_destroy (&bar->mutex);
+        free(bar);
+        errno = errtmp;
+        return -1;
+    }
+
+    //initialise the second semaphore used
+    if(sem_init(&bar->turnstile2, 0, 1) < 0){
+        errtmp = errno;
+        sem_destroy (&bar->turnstile1);
+        pthread_mutex_destroy (&bar->mutex);
+        free(bar);
+        errno = errtmp;
+        return -1;
+    }
+
+    //set the amount of threads (overall) to synchronise
+    bar->th_nb = nb;
+
     return 0;
 }
 
