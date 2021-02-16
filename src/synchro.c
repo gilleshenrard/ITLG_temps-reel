3d541d4cd34bb64d55bbce053e7f06367870d184
Gilles Henrard <gilles.henrard@hotmail.com>
2021-02-16 15:48:14 +0000

    make turnstiles initialised with the proper values

--- src/synchro.c
+++ src/synchro.c
@@ -17,7 +17,7 @@ int barrier_alloc(barrier_t* bar){
     bar = calloc(1, sizeof(barrier_t));
     if (bar){
         pthread_mutex_init(& bar->mutex, NULL);
-        sem_init (&bar->turnstile1, 0, 1);
+        sem_init (&bar->turnstile1, 0, 0);
         sem_init (&bar->turnstile2, 0, 1);
     }
     return 0;
