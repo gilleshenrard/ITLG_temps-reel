e1f5f7f99e9b9e9e636ec145c8d54ca32e2bca90
Gilles Henrard <gilles.henrard@hotmail.com>
2021-02-17 14:17:57 +0100

    prepare the runner structure

--- include/runner.h
+++ include/runner.h
@@ -1,7 +1,14 @@
 #ifndef RUNNER_THREAD_INCLUDED
 #define RUNNER_THREAD_INCLUDED
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 #include "synchro.h"
+
+typedef struct{
+    uint16_t    threadNum;
+    barrier_t   barrier;
+}runner_t;
 
 void *runner_handler(void *num);
 #endif
