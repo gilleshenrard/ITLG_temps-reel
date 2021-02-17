522565192d7c673488681617d3c3b7a374b58664
Gilles Henrard <gilles.henrard@hotmail.com>
2021-02-17 18:58:58 +0000

    move randomisation to main

--- src/runner.c
+++ src/runner.c
@@ -14,16 +14,20 @@
 /*			- wait for a random timespan between 2000 and 5000 us						*/  
 /*  O : /                                                                               */
 /****************************************************************************************/
+/*	WARNING : srand() must already have been called beforehand							*/
+/****************************************************************************************/
 void *runner_handler(void *run)
 {
-	printf("inside a thread\n");
 	runner_t* runner = (runner_t*)run;
 	uint32_t waittime = 0;
 
-	srand(time(NULL));
-	waittime = 2001 + (rand_r((uint32_t*)(&runner->threadNum)) % 2999);
-
-	printf("Thread N%hd starts the step 1 (%d us of sleep time)\n", runner->threadNum, waittime);
+	//sleep for a random amount of usec
+	printf("Thread N%hd starts the step 1\n", runner->threadNum);
+	waittime = 2000 + (rand() % 3000);
 	usleep(waittime);
+	
+	//reach the barrier
+	barrier_sync(runner->barrier);
+	
 	pthread_exit(NULL);
 }
