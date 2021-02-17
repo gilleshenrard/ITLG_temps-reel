546915fea00970af46c568500ab9b050a5801d33
Gilles Henrard <gilles.henrard@hotmail.com>
2021-02-17 14:17:25 +0100

    make each thread wait for random amount of time

--- src/runner.c
+++ src/runner.c
@@ -4,19 +4,25 @@
 **      (as described in the assignment)
 ** ----------------------------------------------------
 ** Made by Gilles Henrard
-** Last modified : 16/02/2021
+** Last modified : 17/02/2021
 */
 #include "runner.h"
 
 /****************************************************************************************/
 /*  I : /                                                                               */
-/*  P : Execute the behaviour of a runner                                               */
+/*  P : Execute the behaviour of a runner :                                             */
+/*			wait for a random timespan between 2000 and 5000 us							*/  
 /*  O : /                                                                               */
 /****************************************************************************************/
 void *runner_handler(void *num)
 {
-	uint16_t thNum = *((int*)num);
+	uint16_t thNum = *((uint16_t*)num);
+	uint32_t waittime = 0, tmp = (uint32_t)thNum;
 
-	printf("Thread %hd starts the step 1\n", thNum);
+	srand(time(NULL));
+	waittime = 2001 + (rand_r(&tmp) % 2999);
+
+	printf("Thread N%hd starts the step 1 (%d us of sleep time)\n", thNum, waittime);
+	usleep(waittime);
 	pthread_exit(NULL);
 }
