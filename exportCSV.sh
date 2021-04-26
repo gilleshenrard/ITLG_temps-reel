e82720cdf4a0fe54c5f0189eb5fd55a7a6d8a561
Gilles Henrard <gilles.henrard@hotmail.com>
2021-04-26 13:02:58 +0200

    make the test run 98 threads altogether

--- exportCSV.sh
+++ exportCSV.sh
@@ -3,7 +3,7 @@
 ##  ----------------------------------------------
 ##
 ##  Author : Gilles Henrard
-##  Last edit : 23/04/2021
+##  Last edit : 26/04/2021
 ##
 ##  Script which runs bin/readerswriters 10 times, counts the amount of times
 ##    readers had the occasion to write an output (as opposed to the writers),
@@ -23,23 +23,23 @@ mkdir -p stat
 for i in {1..100} ; do
     #run readerswriters without nice and export the output in a temp file
     echo "without scheduling, run #$i"
-    bin/readerswriters 19 4 40 none > stat/rw_nosched_19-4-40.txt
+    bin/readerswriters 98 5 30 none > stat/rw_nosched_98-5-30.txt
 
     #run readerswriters with nice and export the output in a temp file
     echo "with nice(), run #$i"
-    bin/readerswriters 19 4 40 nice 1 10 > stat/rw_nice_19-4-40.txt
+    bin/readerswriters 98 5 30 nice 1 10 > stat/rw_nice_98-5-30.txt
 
-    #run readerswriters with nice and export the output in a temp file
+    #run readerswriters with fifo and export the output in a temp file
     echo "with FIFO, run #$i"
-    bin/readerswriters 19 4 40 fifo > stat/rw_fifo_19-4-40.txt
+    bin/readerswriters 98 5 30 fifo > stat/rw_fifo_98-5-30.txt
 
     #compute the amount of readers outputs with and without nice and append both in CSV files
-    cat stat/rw_nosched_19-4-40.txt | grep -i r | wc -l >> stat/rw_nosched_19-4-40.csv
-    cat stat/rw_nice_19-4-40.txt | grep -i r | wc -l >> stat/rw_nice_19-4-40.csv
-    cat stat/rw_fifo_19-4-40.txt | grep -i r | wc -l >> stat/rw_fifo_19-4-40.csv
+    cat stat/rw_nosched_98-5-30.txt | grep -i r | wc -l >> stat/rw_nosched_98-5-30.csv
+    cat stat/rw_nice_98-5-30.txt | grep -i r | wc -l >> stat/rw_nice_98-5-30.csv
+    cat stat/rw_fifo_98-5-30.txt | grep -i r | wc -l >> stat/rw_fifo_98-5-30.csv
 done
 
 #delete temp files
-rm stat/rw_nosched_19-4-40.txt
-rm stat/rw_nice_19-4-40.txt
-rm stat/rw_fifo_19-4-40.txt
+rm stat/rw_nosched_98-5-30.txt
+rm stat/rw_nice_98-5-30.txt
+rm stat/rw_fifo_98-5-30.txt
