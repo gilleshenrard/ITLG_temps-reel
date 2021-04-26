3a9d0bca59ae73350aa20aff9a859440f19ad538
Gilles Henrard <gilles.henrard@hotmail.com>
2021-04-26 16:56:07 +0200

    add algo argument to all tests + implement rw prior tests

--- exportCSV.sh
+++ exportCSV.sh
@@ -22,24 +22,30 @@ mkdir -p stat
 #to be ran 10 times
 for i in {1..100} ; do
     #run readerswriters without nice and export the output in a temp file
-    echo "without scheduling, run #$i"
-    bin/readerswriters 98 5 30 none > stat/rw_nosched_98-5-30.txt
+    echo "No starve without scheduling, run #$i"
+    bin/readerswriters nostarve 98 5 30 none > stat/rw_nosched_98-5-30.txt
 
     #run readerswriters with nice and export the output in a temp file
-    echo "with nice(), run #$i"
-    bin/readerswriters 98 5 30 nice 1 10 > stat/rw_nice_98-5-30.txt
+    echo "No starve with nice(), run #$i"
+    bin/readerswriters nostarve 98 5 30 nice 1 10 > stat/rw_nice_98-5-30.txt
 
     #run readerswriters with fifo and export the output in a temp file
-    echo "with FIFO, run #$i"
-    bin/readerswriters 98 5 30 fifo > stat/rw_fifo_98-5-30.txt
+    echo "No starve with FIFO, run #$i"
+    bin/readerswriters nostarve 98 5 30 fifo > stat/rw_fifo_98-5-30.txt
+
+    #run readerswriters with writers priority and fifo scheduling and export the output in a temp file
+    echo "Writers priority with FIFO, run #$i"
+    bin/readerswriters prior 98 5 30 fifo > stat/rw_prior_fifo_98-5-30.txt
 
     #compute the amount of readers outputs with and without nice and append both in CSV files
     cat stat/rw_nosched_98-5-30.txt | grep -i r | wc -l >> stat/rw_nosched_98-5-30.csv
     cat stat/rw_nice_98-5-30.txt | grep -i r | wc -l >> stat/rw_nice_98-5-30.csv
     cat stat/rw_fifo_98-5-30.txt | grep -i r | wc -l >> stat/rw_fifo_98-5-30.csv
+    cat stat/rw_prior_fifo_98-5-30.txt | grep -i r | wc -l >> stat/rw_prior_fifo_98-5-30.csv
 done
 
 #delete temp files
 rm stat/rw_nosched_98-5-30.txt
 rm stat/rw_nice_98-5-30.txt
 rm stat/rw_fifo_98-5-30.txt
+rm stat/rw_prior_fifo_98-5-30.txt
